ed33d6ab35370c89ff13efb3b72a87dfc65bfbaa
Joseph Varacalli <58127285+joevaracalli@users.noreply.github.com>
2024-12-03 17:14:33 -0500

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,13 +1,19 @@
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 
-const demomode = false;
+const demomode = true;
 const demobox = demomode ? styles.demo.hero : {};
 
 const Work = () => {
   return (
     <div>
-      Joe's Div
+  
+      <iframe src="https://open.spotify.com/embed/album/74SkeKthUN5qzEck1ufEHr?utm_source=generator&theme=0"
+       width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+        
+      
+
     </div>
   );
 };
